8bc7294ea4487167a31b87404c61d6d0e8700cd0
Marco Aurelio Reis Lima Menezes <42839553+marcoaureliomenezes@users.noreply.github.com>
2019-09-03 23:16:48 +0200

    List of difficulty levels as global variable and function printEnunList() implemented, to show the gamer the options he has to choose.

--- Verb.py
+++ Verb.py
@@ -5,9 +5,19 @@ Created on Tue Sep  3 23:02:21 2019
 @author: Marco Menezes
 """
 
-class Verb():
-    
+lvlList = ("Alle Verben (Expert)","Verben pro Klasse (Medium)",
+           "Verben pro Unterklasse (Easy)")
+
+class Verb():   
     def __init__(self):
         pass
+    
+######################### INTERFACE WITH THE GAMER ##### #######################
+    def printEnumList(self,List):
+        confList = []
+        for i in range(0,len(List)):
+            print(i+1, "-", List[i])
+            confList.append(str(i+1))
+        return confList
     
     
