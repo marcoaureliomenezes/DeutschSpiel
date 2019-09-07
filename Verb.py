441f8dab85e2516315906f0407e30657dc0d35f5
Marco Aurelio Reis Lima Menezes <42839553+marcoaureliomenezes@users.noreply.github.com>
2019-09-07 19:07:03 +0200

    functions of interface padronized, creation of the function choseTrans() that gives to the verblist the the language that will follow each verb, implementation of the functions choseSubTitle(), where if the user chose the easy mode he can with this function the chose the subThema he wants to play. This suthema is applied to the verblist created by the game. There are some tasks to realize yet but now the game gives interfaced with the player in order that he can choose the configurations and the list of verbs that will be part of the game is created.

--- Verb.py
+++ Verb.py
@@ -10,23 +10,45 @@ import xlrd
 lvlList = ("Alle Verben (Expert)","Verben pro Klasse (Medium)",
            "Verben pro Unterklasse (Easy)")
 invalidInput = "Bitte geben Sie eine gültige Nummer ein!\n"
+difficultyMsg = "Wählen Sie eine Schwierigkeitsgrad (Nummer) für das VerbenSpiel:\n\n"
+mutterspracheMsg = "\nWählen Sie bitte eine Muttersprache für die Übersetzung:\n\n"
+subThemaMsg = "\nWählen Sie bitte eine Nummer für das Untertitel des Spiels:\n\n"
+themaMsg = "\nWählen Sie bitte eine Nummer für das Thema des Spiels:\n\n"
+firstMsg = "\n\nHallo mein Freund! Herzlich Willkommen zum Spiel von Marco Menezes.\n\n"
 deutschspreadsheet = xlrd.open_workbook('Deutsch_Databank.xlsx')
 VerbenSheet = deutschspreadsheet.sheet_by_name('Verben')
 
 class Verb():
     def __init__(self):
         pass   
- 
+#------------------------------------------------------------------------------#  
     def Cell(self,line,column):
         return VerbenSheet.cell(line,column).value
-    
-    def refLabel(self,name):
-        for i in range(0,200):
-            if name == Verb.Cell(self,0,i):
-                return i
-
+#------------------------------------------------------------------------------#     
+    def refCLabel(self,label):
+        count = 0
+        while 1:
+            if label == Verb.Cell(self,0,count):
+                break
+            if Verb.Cell(self,0,count) == "end":
+                print("Es gibt keine Label heißt", label)
+                break
+            count += 1
+        return count
+#------------------------------------------------------------------------------#
+    def refSubTitles(self,name):
+        lista = []
+        for i in range(0,4):
+            for j in range(0,100):
+                if Verb.Cell(self,i,j) == name:
+                    count = 1
+                    while Verb.Cell(self,i,j + count) == '':
+                        count += 1
+                    lista.append([(i,j),count])
+        return lista
+#------------------------------------------------------------------------------# 
     def numVerbs(self, title):        
-        vRef = Verb.refLabel(self,title)
+        vRef = Verb.refCLabel(self,title)
         for i in range(1,5):
             if Verb.Cell(self,i,vRef+1) == "":
                 initLine = i+1
@@ -52,21 +74,27 @@ class Verb():
         return titleList
 #------------------------------------------------------------------------------#
             
-######################### ACCESS TO THE DYNAMIC DATABASE #######################
-    
-    def callVerbList(self,level):     
+######################### ACCESS TO THE DYNAMIC DATABASE #######################   
+    def callVerbList(self,level):  
+        verbList = []
+        translate = Verb.choseTrans(self) 
         if level == lvlList[0]:
-            pass        
+            titles = Verb.gettitles(self)
+            for title in titles:
+                verbList += Verb.createVLC(self,title,translate)
         elif level == lvlList[1]:
-            inputPar = Verb.paramCreateLists(self)
-            Verb.createVLC(self,inputPar[0],inputPar[1])
+            titles = Verb.gettitles(self)
+            title = Verb.choseTitle(self,titles)
+            verbList = Verb.createVLC(self,title,translate)
         elif level == lvlList[2]:
-            inputPar = Verb.paramCreateLists(self)
+            titles = Verb.gettitles(self)
+            title = Verb.choseTitle(self,titles)
+            verbList = Verb.createVLSC(self,title,translate)
+        return verbList
+        
 #------------------------------------------------------------------------------#    
-    def createVLC(self):
-        titles = Verb.gettitles(self)
-        title = Verb.choseTitle(self,titles)
-        cRef = Verb.refLabel(self,title)
+    def createVLC(self, title, translate):
+        cRef = Verb.refCLabel(self,title) 
         VList = []
         lList = []
         refSize = Verb.numVerbs(self,title)
@@ -77,17 +105,32 @@ class Verb():
             if Verb.Cell(self,i,cRef) != '':
                 if Verb.Cell(self,i,cRef+1) != '':
                     lList.append(Verb.Cell(self,i,cRef))
+                    lList.append(Verb.Cell(self,i,cRef + translate))
                 else:
                     continue
             VList.append(lList)
         return VList
+#------------------------------------------------------------------------------#
+    def createVLSC(self,title,MT):
+        VList = []
+        lList = []
+        miniList = Verb.choseSubTitle(self,title)
+        refList = Verb.refCLabel(self,title)
+        for i in range(miniList[0],miniList[1]):
+            lList = []
+            rL = refList
+            lList.append(Verb.Cell(self,i,rL))
+            lList.append(Verb.Cell(self,i,rL + MT))
+            VList.append(lList)
+        return VList
+#------------------------------------------------------------------------------#
+
         
 ######################### INTERFACE WITH THE GAMER #############################
     def welcome(self):
-        print("Hallo mein Freund!",
-          "Herzlich Willkommen zum Spiel von Marco Menezes.\n\n")
+        print(firstMsg)
         time.sleep(5)
-        print("Wählen Sie eine Schwierigkeitsgrad (Nummer)\n\n")    
+        print(difficultyMsg)
 #------------------------------------------------------------------------------#    
     def printEnumList(self,List):
         confList = []
@@ -105,19 +148,96 @@ class Verb():
             if inputLvl in confList:
                 break
             print(invalidInput)
+            time.sleep(2)
         level = lvlList[int(inputLvl)-1]
+        print("Schwierigkeitgrad: ", level)
+        time.sleep(2)
         return level
 #------------------------------------------------------------------------------#    
     def choseTitle(self, titles):
-
-        print("Wählen Sie eine Nummer für das Thema des Spiels!\n\n")
-        confList = Verb.printEnumList(self,titles)
-        thema = ''
-        while thema not in(confList):
-            print(invalidInput)            
-            thema = input()
-        return titles[int(thema) - 1]
+        print(themaMsg)
+        inputThema = 0
+        while 1:
+            confList = Verb.printEnumList(self,titles)
+            inputThema = input("Thema: ")
+            if inputThema in confList:
+                break
+            print(invalidInput) 
+            time.sleep(2)
+        thema = titles[int(inputThema) - 1]
+        print("Thema: ",thema)
+        time.sleep(2)
+        return thema
+#------------------------------------------------------------------------------#    
+    def choseSubTitle(self,title):
+        tRef = Verb.refCLabel(self,title)
+        subTitle = []
+        Interval = []
+        count = 1
+        line = " "
+        while(1):
+            line = Verb.Cell(self,count,tRef)
+            if line == "":
+                break
+            if Verb.Cell(self,count,tRef+1) == "":
+                subTitle.append(Verb.Cell(self,count,tRef))
+            count += 1
+            
+        print(subThemaMsg)
+        sTitle = 0
+        while 1: 
+            confList = Verb.printEnumList(self,subTitle)
+            inputST = input("UnterThema: ")
+            if inputST in confList:
+                break
+            print(invalidInput)
+            time.sleep(2)
+            
+        inputST = int(inputST)
+        
+        if inputST == len(confList):
+            print("ta no limite")  #TODO: RESOLVE THIS LIMIT FAST
+        else:
+            interval = (subTitle[inputST-1],subTitle[inputST])            
+            count2 = 3
+            line = " "
+            while line != "":
+                line = Verb.Cell(self,count2,tRef)
+                if line == interval[0]:
+                    Interval.append(count2+1)
+                if line == interval[1]:
+                    Interval.append(count2)
+                count2 += 1
+            return Interval  
 #------------------------------------------------------------------------------#
-
+    def choseTrans(self):
+        tongue =[]
+        tRef = Verb.refSubTitles(self,"Übersetzung")
+       # for i in tRef: TODO: CHECK THE TRANSLATE COLUMNS PER TITLE.
+       #     print(i[1])
+        line = ((tRef[0])[0])[0]
+        col = ((tRef[0])[0])[1]
+        size = (tRef[0])[1]       
+        for i in range(0,size):
+            tongue.append(Verb.Cell(self,line+1,col+i))             
+        print(mutterspracheMsg)
+        inputMT = 0
+        while 1:
+            confList = Verb.printEnumList(self,tongue)
+            inputMT = input("Muttersprache: ")
+            if inputMT in confList:
+                break
+            print(invalidInput)
+            time.sleep(2)
+                       
+        inputMT = int(inputMT)
+        print("Muttersprache: ",tongue[inputMT-1])
+        time.sleep(2)
+        return inputMT
+    
+################################################################################
 player1 = Verb()
-print(player1.createVLC())
+level = player1.choseSizeList()
+verbList = player1.callVerbList(level)
+for i in verbList:
+    print(i)
