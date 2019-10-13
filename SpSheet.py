1a40e4f097b9b15204d1528507fca8cfa8729c22
Marco Aurelio Reis Lima Menezes <42839553+marcoaureliomenezes@users.noreply.github.com>
2019-10-13 11:07:06 +0200

    Alterations made in the file Spreadsheet, that now is just a fulibrary that suports the classes Nomen, Verb and Wortschatz. The functions of this library are generalizated to supply all the classes.

--- SpSheet.py
+++ SpSheet.py
@@ -1,146 +1,253 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep 10 00:05:06 2019
-
 @author: Marco Menezes
 """
 import xlrd
 import time
+import texts as txt
 
-mutterspracheMsg = "\nWählen Sie bitte eine Muttersprache für die Übersetzung:\n\n"
-invalidInput = "Bitte geben Sie eine gültige Nummer ein!\n"
-themaMsg = "\nWählen Sie bitte eine Nummer für das Thema des Spiels:\n\n"
+'''
+remDupList(List) function receives as parameter a list and returns the content
+of this list without duplicate data.
+'''
+def remDupList(List):
+    lista2 = []
+    for i in List:
+        if i not in lista2:
+            lista2.append(i)
+    return lista2
+'''
+The accessSP(sheet) function receives as parameter the sheet's name and returns
+this sheet. 
+'''
+def accessSP(sheet):
+    DeutschSpreadsheet = xlrd.open_workbook('Deutsch_Databank.xlsx')
+    Sheet = DeutschSpreadsheet.sheet_by_name(sheet)
+    return Sheet
+'''
+The Cell(sheet,line,column function receives as parameters the sheet's name,
+line and column and returns the content of the respective cell. 
+'''
+def Cell(Sheet,line,column):
+    return Sheet.cell(line,column).value
+'''
+initLine method receives as parameter a column and a preValue (the word in the
+cell that  and returns the first value
+(word) of that column.
+'''
+def initLine(sheet,col,preValue):
+    count = 0
+    while Cell(sheet,count,col) != preValue:
+        count += 1
+    return count + 1
+'''
+The printEnumList(List) function receives as parameter a List, prints the list 
+content enumered to the player as options to choose and returns a list with
+these numbers.
+z.B: A list contains 3 options to choose. the function returns the list (1,2,3).
+After in the interface functions the player will be restricted to choose one of
+these numbers as option. Any other choice will be not accept.
+'''    
+def printEnumList(List):
+    confList = []
+    for i in range(0,len(List)):
+        print(i+1, "-", List[i])
+        confList.append(str(i+1))
+    return confList
+'''
+The refCTitle(sheet,label) function receives as parameters the sheet's name
+and a label, both are string variables. Then the function searches for the title
+required in the cells and if the label is found it'll be return the column.
+'''
+def refCTitle(sheet,label):
+    count = 0
+    while 1:
+        if label == Cell(sheet,0,count):
+            break
+        if Cell(sheet,0,count) == "end":
+            print("Es gibt keine Label heißt", label)
+            break
+        count += 1
+    return count
+'''
+The getSubtitles(sheet,col) function receives as parameter the sheet's name and
+a column and runs though the lines. At the end it returns a list containing the
+subtitles of that list.
+'''
 
+def getSubtitles(sheet,col):
+    STList = []
+    line1 = initLine(sheet,col,'Genre')
+    value = ' '
+    while 1:
+        value = Cell(sheet,line1,col)
+        if value == '':
+            break
+        if Cell(sheet,line1,col+1) == '':
+            STList.append(value)
+        line1 += 1
+    return STList
+'''
+The getTitles(sheet) function receives as parameter the name of the sheet and
+returns a list containing these titles
+'''
+def getTitles(sheet):
+    titleList = []
+    count = 0
+    while Cell(sheet,0,count) != 'end':
+        if Cell(sheet,0,count) != '':
+            titleList.append(Cell(sheet,0,count))
+        count += 1
+    return titleList
+'''
+The choseTitle(titles) fuction receives as parameter a list of titles, prints
+a message and the options that the player has enumered.  
+'''       
+def showTitle(titles):
+    confirmList = []
+    print(txt.themaMsg)
+    for i in range(0,len(titles)):
+        confirmList.append(str(i+1))
+        print(str(i+1),"-", titles[i])
+    return confirmList
 
-class SpSheet():   
-    def __init__(self):
-        pass
-#------------------------------------------------------------------------------#   
-    def accessSP(aba):
-        deutschspreadsheet = xlrd.open_workbook('Deutsch_Databank.xlsx')
-        Aba = deutschspreadsheet.sheet_by_name(aba)
-        return Aba
+'''
+The choseTitle(titles) function is a method of interface. It has as parameter a 
+list of titles and asks the player to choose one of the (only one). Finally
+it returns the name of the title chosen. 
+'''
+def choseTitle(titles): 
+    thema = ''
+    confList = showTitle(titles)  
+    while 1:
+        print(txt.invalidInput)            
+        thema = input()
+        if thema in confList:
+            break
+        print(txt.invalidInput)
+    return titles[int(thema)-1]
 
-#------------------------------------------------------------------------------#  
-    def Cell(self,Aba,line,column):
-        return Aba.cell(line,column).value
-#------------------------------------------------------------------------------#    
-    def printEnumList(self,List):
-        confList = []
-        for i in range(0,len(List)):
-            print(i+1, "-", List[i])
-            confList.append(str(i+1))
-        return confList
-#------------------------------------------------------------------------------#     
-    def refCLabel(self,aba,label):
+'''
+The choseTitles(titles) function is similar to the choseTitle, but with the
+difference the player can choose more than one title from the list.
+'''  
+def choseTitles(titles):
+    returnList = []
+    thema = ''
+    confirmList = showTitle(titles)  
+    while 1:
         count = 0
-        while 1:
-            if label == SpSheet.Cell(self,aba,0,count):
-                break
-            if SpSheet.Cell(self,aba,0,count) == "end":
-                print("Es gibt keine Label heißt", label)
-                break
-            count += 1
-        return count
-#------------------------------------------------------------------------------#
-    def refSubTitles(self,aba,name):
-        lista = []
-        for i in range(0,4):
-            for j in range(0,100):
-                if SpSheet.Cell(self,aba,i,j) == name:
-                    count = 1
-                    while SpSheet.Cell(self,aba,i,j + count) == '':
-                        count += 1
-                        if SpSheet.Cell(self,aba,i+2,j + count) == '':
-                            break
-                    ref = [(i,j),count]
-        return ref
-#------------------------------------------------------------------------------#
-    def gettitles(self,aba):
-        titleList = []
-        for i in range(0,200):
-            if SpSheet.Cell(self,aba,0,i) == 'end':
-                break
-            if SpSheet.Cell(self,aba,0,i) != '':
-                titleList.append(SpSheet.Cell(self,aba,0,i))
-        return titleList
-    
-#------------------------------------------------------------------------------#   
-    '''Pseudocodigo
-    recebe referencia Wortschatz [[]]
-    define linha, coluna e largura
-    
-    
-    '''
-    def choseTrans(self,aba,ref):
-        tongue =[]
-        tongueAddr =[]
-        col = (ref[0])[1]
-        lineKind = (ref[0])[0] + 1
-        Tline = lineKind + 1
-        size = ref[1]
-        
-        for i in range(0,size):
-            if SpSheet.Cell(self,aba,lineKind,col + i) == "Übersetzung":
-                count = 0
-                tCol = col + i
-                while SpSheet.Cell(self,aba,Tline,tCol) != '':                   
-                    tongueName = SpSheet.Cell(self,aba,Tline,tCol)
-                    tongue.append((tongueName))
-                    tongueAddr.append(tCol)
+        print(txt.invalidInput)            
+        thema = input() 
+        thema = thema.replace(" ","")
+        themaList = thema.split(",")
+        themaList = remDupList(themaList)
+        for i in themaList:
+            for j in confirmList:
+                if i == j:
                     count += 1
-                    tCol = col + i + count
-                    
-        inputMT = 0
-        while 1:
-            confList = SpSheet.printEnumList(self,tongue)
-            inputMT = input("Muttersprache: ")
-            if inputMT in confList:
-                break
-            print(invalidInput)                    
-        inputMT = int(inputMT)
-        print("Muttersprache: ",tongue[inputMT-1])
-        return tongueAddr[inputMT-1]
-#------------------------------------------------------------------------------#      
-    def firstLine(self,aba,vref,preValue):
-        count = 0
-        ref = (vref[0])[1]
-        zeroValue = ''
-        while zeroValue != preValue:
-            zeroValue = SpSheet.Cell(self,aba,count,ref)
-            count += 1
-        return count
+        if count == len(themaList):
+            break
+    for i in themaList:
+        returnList.append(titles[int(i)-1])                
+    return returnList    
+#------------------------------------------------------------------------------#
+'''
+The refSubTitles(sheet,name) function receives as parameter the sheet's name
+and the name of the title searched. Then it runs through the lines and columns
+and when it found the title it returns a list of 2 elements (address,size).
+Address means the coordinate (i,j) and size the width (in number of columns)
+of that title. 
+'''
+def refTitle(sheet,name):
+    for i in range(0,40):
+        for j in range(0,20):
+            if Cell(sheet,i,j) == name:
+                size = 1
+                while Cell(sheet,i,j + size) == '':
+                    size += 1
+                    if (Cell(sheet,i+1,j + size) == '' and 
+                        Cell(sheet,i+2,j + size) == ''):
+                        break
+                return [(i,j),size]
+'''
+
+'''
+def refSubTitleList(sheet,List):
+    RefList = []
+    for i in range(0,len(List)):
+        RefList.append(refTitle(sheet,List[i]))
+    return RefList
+#------------------------------------------------------------------------------#
+'''
+The refTrans(sheet,ref) function receives as parameter a sheet and a reference
+(a column). Then it gets the parameters of the reference, finds the column
+which starts the translation languages and puts the language names contained in
+a list called tongue and the respective address (column) of these languages in
+a list called tongueAdress.
+Finally 
+'''
+def choseTrans(sheet,ref):
+    tongue =[]
+    tongueAddr =[]
+    line = (ref[0])[0]
+    col = (ref[0])[1]   
+    lineST = (ref[0])[0] + 1   # Line that contains the subtitles
+    lineLang = line + 2        # Line that contains the languages
+    size = ref[1]              # Size of the table.
+
+    for i in range(0,size):
+        if Cell(sheet,lineST,col + i) == "Übersetzung":
+            count = 0
+            tCol = col + i
+            while Cell(sheet,lineLang,tCol) != '':                   
+                tongueName = Cell(sheet,lineLang,tCol)
+                tongue.append((tongueName))
+                tongueAddr.append(tCol)
+                count += 1
+                tCol = col + i + count              
+    inputMT = 0
+    while 1:
+        confList = printEnumList(tongue)
+        inputMT = input("Muttersprache: ")
+        if inputMT in confList:
+            break
+        print(txt.invalidInput)                    
+    inputMT = int(inputMT)
+    return tongueAddr[inputMT-1]
+'''
+The firstLine(sheet,vref,preValue) receives as parameter the sheet's name, a
+column where the table is (ref) and the preValue, that indicates the content
+of the cell that precedes the first value of that table.
+'''    
+def firstLine(sheet,ref,preValue):
+    count = 0
+    ref = (ref[0])[1]
+    zeroValue = ''
+    while zeroValue != preValue:
+        zeroValue = Cell(sheet,count,ref)
+        count += 1
+    return count
 #------------------------------------------------------------------------------#    
-    def numElem(self,aba, vref):        
-
-        for i in range(1,5):
-            if SpSheet.Cell(self,aba,i,vref+1) == "":
-                initLine = i+1
-                break
-        count = 0
-        while(1):
-            if SpSheet.Cell(self,aba,count,vref) == '':
-                finalLine = count
-                break
-            count += 1
-        return [initLine,finalLine]
-
+def numElem(aba, vref):        
+    for i in range(1,5):
+        if Cell(aba, i, vref+1) == "":
+            initLine = i+1
+            break
+    count = 0
+    while(1):
+        if Cell(aba,count,vref) == '':
+            finalLine = count
+            break
+        count += 1
+    return [initLine,finalLine]
 #------------------------------------------------------------------------------#
-        
-    def choseTitle(self, titles):
-        confirmList = []
-        print(themaMsg)
-        for i in range(0,len(titles)):
-            confirmList.append(str(i+1))
-            print(str(i+1),"-", titles[i])
-        thema = ''
-        while thema not in(confirmList):
-            print(invalidInput)            
-            thema = input()        
-        intThema = int(thema)
-        return titles[intThema - 1]
-    
-#------------------------------------------------------------------------------#
-play = SpSheet()
-aba = SpSheet.accessSP("Nomen")
-
+#TODO: 
+# Escolher todos
+# Tirar na sorte pronome
+# Tirar na sorte substantivo
+#Tirar na sorte caso    
+# Começar a criar modelos de expressões para gerar sentenças  
+#Terminar Wortschatz  
+# Deklinação de adjetivos  
