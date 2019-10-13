5fecee76d853c51bea9c1ad0d7fb95f2b34154c4
Marco Aurelio Reis Lima Menezes <42839553+marcoaureliomenezes@users.noreply.github.com>
2019-10-13 11:11:27 +0200

    Class Wortschatz with all the functions needed and TODOs also needed to make the class ready for a apresentation to Stork Tronik .

--- WortSchatz.py
+++ WortSchatz.py
@@ -4,32 +4,32 @@ Created on Sun Sep 15 13:47:43 2019
 
 @author: Marco Menezes
 """
-from SpSheet import SpSheet
+import SpSheet as Sp
 import time
 import random
 
-aba = SpSheet.accessSP("WortSchatz") 
+wortS = Sp.accessSP("WortSchatz") 
 
-class WortSchatz(SpSheet):
+class WortSchatz:
     
+    '''
+    Constructor method without parameters.
+    '''
     def __init__(self):
-        super().__init__()
-#------------------------------------------------------------------------------#        
-    def cell():
-        pass
-#------------------------------------------------------------------------------# 
-    def initLine(self,col):
-        count = 0
-        while SpSheet.Cell(self,aba,count,col) != "Singular/Präsens":
-            count += 1
-        return count + 1
-            
+        super().__init__()      
+#------------------------------------------------------------------------------#
+
 #------------------------------------------------------------------------------#         
+    '''
+    ChoseWS method creates a list with the titles from the Wortschatz lists
+    that the spreadsheet contains, calls the function choseTitle, in order to
+    the player choose what we wants to play an returns the address reference of
+    this Wortschatz list.
+    '''
     def ChoseWS(self): 
-        lista = SpSheet.gettitles(self,aba)
-        titleChose = SpSheet.choseTitle(self,lista)
-        print(titleChose)
-        return SpSheet.refSubTitles(self,aba,titleChose)
+        lista = Sp.gettitles(wortS)
+        titleChose = Sp.choseTitle(lista)
+        return Sp.refSubTitles(wortS,titleChose)
 #------------------------------------------------------------------------------#
     ''' Method CrtSingList() (create a singular list):
         
@@ -38,16 +38,16 @@ class WortSchatz(SpSheet):
         wortList = []
         ref = WortSchatz.ChoseWS(self)
         col = (ref[0])[1]
-        colT = SpSheet.choseTrans(self,aba,ref)
+        colT = Sp.choseTrans(wortS,ref)
         colTip = col + 2
-        line = SpSheet.firstLine(self,aba,ref,"Singular/Präsens")
+        line = Sp.firstLine(wortS,ref,"Singular/Präsens")
 
-        while SpSheet.Cell(self,aba,line,col) != '':
+        while Sp.Cell(wortS,line,col) != '':
             elemList = []
-            elemList.append(SpSheet.Cell(self,aba,line,col))
-            elemList.append(SpSheet.Cell(self,aba,line,col+1))
-            elemList.append(SpSheet.Cell(self,aba,line,colTip))
-            elemList.append(SpSheet.Cell(self,aba,line,colT))
+            elemList.append(Sp.Cell(wortS,line,col))
+            elemList.append(Sp.Cell(wortS,line,col+1))
+            elemList.append(Sp.Cell(wortS,line,colTip))
+            elemList.append(Sp.Cell(wortS,line,colT))
             wortList.append(elemList)
             line += 1
         return wortList
@@ -67,14 +67,17 @@ class WortSchatz(SpSheet):
                 input()
 #------------------------------------------------------------------------------#
     def subTraining(self,elem):
-        ans = input(elem[1] + ": ")
-        if ans == elem[0]:
+        provSub = elem[0].split(' ')
+        print(provSub[1] + ", Plural: ", elem[1])
+        ans = input("Article: ")
+        if ans == provSub[0]:
             return True
         else:
             return False
 #------------------------------------------------------------------------------#    
     def verbTraining(self,elem):
-        ans = input((elem)[0] + "\nPerfekt: ")
+        print(elem[0] + ", " + elem[3])
+        ans = input("\nPerfekt: ")
         if ans == (elem)[1]:
             return True
         else:
@@ -82,10 +85,7 @@ class WortSchatz(SpSheet):
 #------------------------------------------------------------------------------#        
     def adTraining(self,elem):
         ans = input((elem)[0] + "\nSynonym: ")
-        if ans == (elem)[1]:
-            return True
-        else:
-            return False       
+        wort = WortSchatz.Synonym(self,elem[1])      
 #------------------------------------------------------------------------------#        
     def atString(self,index,string,repl):
         nString = ''
@@ -118,40 +118,38 @@ class WortSchatz(SpSheet):
 #------------------------------------------------------------------------------#
     def training(self,List):         
             ans = ''
+            print("\n\n")
             while ans != "quit":
                 rand = random.randint(0,len(List)-1)
                 elem = List[rand]
                 if elem[2] == "Substantiv":
-                    provSub = elem[0].split(' ')
-                    answer = WortSchatz.subTraining(self,provSub)
+                    answer = WortSchatz.subTraining(self,elem)
                     print(answer)
                 if elem[2] == "Verb":
                     answer = WortSchatz.verbTraining(self,elem)
                     print(answer)
                 if elem[2] == "Adjektiv" or elem[2] == "Adverb":
-                    answer = WortSchatz.adTraining(self,elem)
-                    print(answer)
-                else:
-                    print(List[rand])
+                    answer = WortSchatz.Synonym(self,elem[1])                    
+
                 ans = input()
-    '''
-            TODO: 
-                * Always print the translation in front of the word before to
-                make the question about perfekt, gender, plural or synonym.
-                * Quit to the Method printList()               
-                * Exercise about gender and plural
-                * synonyms game (put to work)
-                * Perfekt game
-                * Put synonyms in the spreadsheet for wortschatz 1 to 3
-                * Complement specifications of the verbs on the spreadsheet
-                (akk, dat, reflex) (preposition).
-                * Create the function that manage the class Wortschatz()
-                * Create Score of the game
-                * Make the commit commit
-    '''         
+'''
+TODO:
+    1- Training the wortschatz
+        a) Choose the wortschatz you want to training
+        b) Choose the language you want to see the translations
+            Play till the player gives a quit.
+            * If the word is a substantive
+                asks the gender and the plural
+            * If the word is a verb:
+                asks the Perfekt 
+            * If the word is an adverb or adjective:
+                asks the synonym and gives 3 change in the schema "Forca"
+                
+                
+                
+TODO2:
+    Creates the Score
+
+'''         
 #------------------------------------------------------------------------------#
-wort = WortSchatz()
-for i in range(0,1):
-    WortList = wort.CrtSingList()
-    wort.training(WortList)
-            
+wort = WortSchatz()         
